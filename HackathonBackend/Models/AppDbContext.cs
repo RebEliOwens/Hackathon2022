f6105b1775f610c66ecdcd0119ec477798a9bb15
Rebecca Owens <ROwens@tql.com>
2022-07-18 12:00:52 -0400

    init

---
+++ HackathonBackend/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HackathonBackend.Models
+{
+    public class AppDbContext : DbContext
+    {
+        //context goes here
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+    }
+}
